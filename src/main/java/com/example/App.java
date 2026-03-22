9e9218727154dc1fa3880db195016974ae99095a
voliran <dy.290@mail.ru>
2026-03-22 22:58:08 +0300

    Apache Commons Lang3 import to maven

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(StringUtils.reverse("Hello, World!"));
     }
 }
